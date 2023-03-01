b5b54940ff1138715a18a4fb97a2cc234e5eda54
Will Turner <will@thefutureforestcompany.com>
2023-03-01 11:26:06 +0000

    added import os

--- app.py
+++ app.py
@@ -2,12 +2,19 @@ import pandas as pd
 from azure.storage.blob import BlobServiceClient
 import io
 import os
+import streamlit as st
 
 connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
 
-blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-container_client = blob_service_client.get_container_client("globaldataset")
-blob_client = container_client.get_blob_client("regridded_data_v5.csv")
-csv_content = blob_client.download_blob().readall()
-df = pd.read_csv(io.BytesIO(csv_content ))
-print(df)
+@st.cache_data
+def get_data():
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+    container_client = blob_service_client.get_container_client("globaldataset")
+    blob_client = container_client.get_blob_client("regridded_data_v5.csv")
+    csv_content = blob_client.download_blob().readall()
+    df = pd.read_csv(io.BytesIO(csv_content))
+    return df
+
+
+df = get_data()
+st.dataframe(df)
